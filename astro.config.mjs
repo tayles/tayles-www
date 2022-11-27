15908275d7a8efbe4fc86eea0a59044d6015e6bb
David Taylor <dtaylor@evertz.com>
2022-11-27 01:34:57 -0500

    Update eslint/prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tayles.co.uk',
-  output: "server",
+  output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
 });
