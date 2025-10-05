3d86738b8faf883a1b32e8f96b0449123a1b136f
David Taylor <114563+tayles@users.noreply.github.com>
2025-10-05 09:12:13 -0400

    build: Update tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,24 @@ import { manifest, seoConfig } from './utils/seo-config';
 
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'astro/config';
-// Astro integration imports
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: seoConfig.baseURL,
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-        // path: './tailwind.config.cjs',
-      },
-    }),
-  ],
   vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest,
-        workbox: {
-          globDirectory: 'dist',
-          globPatterns: [
-            '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-          ],
-          // Don't fallback on document based (e.g. `/some-page`) requests
-          // This removes an errant console.log message from showing up.
-          navigateFallback: null,
-        },
-      }),
-    ],
+    plugins: [VitePWA({
+      registerType: 'autoUpdate',
+      manifest,
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: [
+          '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+        ],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null,
+      },
+    }), tailwindcss()],
   },
 });
